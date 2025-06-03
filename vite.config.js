5f8af98a82c5edebb6e2d619dc8e15b2cb395b1f
OiseauPython <jeremy.stanko@posteo.net>
2025-06-03 19:13:25 +0200

    fix variable and scss import

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,30 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+const __dirname = new URL('.', import.meta.url).pathname
+
 export default defineConfig(({ mode }) => {
   // Load environment variables based on the current mode
   const env = loadEnv(mode, process.cwd(), '')
   
   return {
     plugins: [vue(), vueJsx()],
-    base: env.NODE_ENV === 'production' ? '/Oisogs/' : '/',
+    base: env.VITE_BASE_URL || '/',
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @import "@/assets/variables.scss";
+            @import "@/assets/mixin.scss";
+          `
+        }
+      }
+    },
     define: {
       'process.env': {}
     },
